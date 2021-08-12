c7b3475b67a6a4e6391640d7ea1b8b1cd8f500a4
Garima <garimak29@gmail.com>
2021-08-12 23:42:06 +0530

    first draft

---
+++ src/main/java/com/BookMyShow/app/Main.java
@@ -0,0 +1,9 @@
+package com.BookMyShow.app;
+
+
+public class Main {
+    String name;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/BookMyShow/app/models/Audit.java
@@ -0,0 +1,12 @@
+package com.BookMyShow.app.models;
+
+
+import java.util.Date;
+
+public class Audit {
+    private Long Id;
+    private Date creationDate;
+    private Date updationDate;
+
+
+}
